830a0be5c3ac14a77a916961d1c5401ab05ab0b3
masood-66 <masoodaslamkhan09@gmail.com>
2024-07-10 14:16:18 +0500

    commit

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+(function($){
+	$(document).ready(function() {	
+
+		// Scroll to Top
+		jQuery('.scrolltotop').click(function(){
+			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+			return false;
+		});
+		
+		jQuery(window).scroll(function(){
+			var upto = jQuery(window).scrollTop();
+			if(upto > 500) {
+				jQuery('.scrolltotop').fadeIn();
+			} else {
+				jQuery('.scrolltotop').fadeOut();
+			}
+		});
+
+
+	// code for menu
+
+	$(".sm-menu-wrapper").click(function (){
+		$(".sm-menu .menu-wrapper").slideToggle();
+	});
+
+	// code for search box
+
+	$(".serach-wrapper input").focus(function (){
+		$(".ser-suggest").show();
+	}).blur(function() {
+    $(".ser-suggest").hide();
+    });
+
+
+	// code for dropdown
+
+	$(".menu-sm-pop ul li").click(function (){
+		$(this).children("ul").slideToggle();
+	})
+
+
+		
+
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+})(jQuery);
